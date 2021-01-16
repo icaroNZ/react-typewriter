a7577d81cf289affabd2b1856d9992a684355990
Icaro <icarolavrador@gmail.com>
2021-01-17 11:21:11 +1300

    Updates

---
+++ precache-manifest.9f7c71ca6b3ee1db831442ca819f8840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70d522bb3c09a0694541946980ffb9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "293fca6ac4382905049c",
+    "url": "./static/css/main.51138d3a.chunk.css"
+  },
+  {
+    "revision": "471aa902e6eb0dd0a576",
+    "url": "./static/js/2.a123bf93.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a123bf93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293fca6ac4382905049c",
+    "url": "./static/js/main.c95affdb.chunk.js"
+  },
+  {
+    "revision": "fc14d89c534bae164484",
+    "url": "./static/js/runtime-main.62962fd7.js"
+  }
+]);

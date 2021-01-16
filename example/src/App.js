b7127eb15d09703b7a84d8ea8a4d7f0a95dc2676
Icaro <icarolavrador@gmail.com>
2021-01-17 11:18:06 +1300

    v.0.0.1

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-typewriter'
+import Typewriter from 'react-typewriter'
 import 'react-typewriter/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const textArrayExample = [
+    'small',
+    'medium, this is a medium size text example',
+    'long text, this is a long text to server as an example how long text work!'
+  ]
+  return <Typewriter textArray={textArrayExample} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useEffect, useRef, useState } from 'react'
+import styles from './styles.module.scss'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+function Typewriter({ textArray }) {
+  if (typeof textArray === 'undefined' || textArray.length === 0) {
+    throw new Error('The prop textArray most have at least one value')
+  }
+
+  useEffect(() => {
+    startType(textArray[0], 0)
+  }, [])
+
+  const typewriter = useRef()
+  const [typed, setTyped] = useState('')
+  const [showHighlight, setShowHighlight] = useState(false)
+  const textLineSize = textArray.length - 1
+
+  let newTyped = ''
+  let textLineIndex = 0
+
+  function startType(pun, index) {
+    if (index < pun.length) {
+      newTyped = newTyped + pun.charAt(index)
+      setTyped(newTyped)
+      index++
+      setTimeout(function () {
+        startType(pun, index)
+      }, 50)
+    } else {
+      setTimeout(function () {
+        setShowHighlight(true)
+      }, 4000)
+
+      setTimeout(function () {
+        setShowHighlight(false)
+        newTyped = ''
+        setTyped(newTyped)
+        textLineIndex++
+        if (textLineIndex > textLineSize) {
+          textLineIndex = 0
+        }
+        startType(textArray[textLineIndex], 0)
+      }, 5000)
+    }
+  }
+
+  return (
+    <span
+      ref={typewriter}
+      className={`${styles.typewriter} ${
+        showHighlight ? styles.highlight : ''
+      }`}
+    >
+      {typed}
+    </span>
+  )
 }
+
+export default Typewriter
